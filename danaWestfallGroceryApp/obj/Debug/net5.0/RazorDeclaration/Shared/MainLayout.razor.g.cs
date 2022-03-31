7424f8addf9d112bc3505d2d6d3fffaad25217fc
danaWestfallScc <dana.westfall77@stclairconnect.ca>
2022-03-31 09:52:14 -0400

    Moved Readme File

--- danaWestfallGroceryApp/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ danaWestfallGroceryApp/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,70 +13,70 @@ namespace danaWestfallGroceryApp.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\danaw\OneDrive - St. Clair College\WestfallDanaWeb315Assignment\danaWestfallGroceryApp\_Imports.razor"
+#line 1 "c:\Users\danaw\OneDrive - St. Clair College\WestfallDanaWeb315Assignment\danaWestfallGroceryApp\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\danaw\OneDrive - St. Clair College\WestfallDanaWeb315Assignment\danaWestfallGroceryApp\_Imports.razor"
+#line 2 "c:\Users\danaw\OneDrive - St. Clair College\WestfallDanaWeb315Assignment\danaWestfallGroceryApp\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\danaw\OneDrive - St. Clair College\WestfallDanaWeb315Assignment\danaWestfallGroceryApp\_Imports.razor"
+#line 3 "c:\Users\danaw\OneDrive - St. Clair College\WestfallDanaWeb315Assignment\danaWestfallGroceryApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\danaw\OneDrive - St. Clair College\WestfallDanaWeb315Assignment\danaWestfallGroceryApp\_Imports.razor"
+#line 4 "c:\Users\danaw\OneDrive - St. Clair College\WestfallDanaWeb315Assignment\danaWestfallGroceryApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\danaw\OneDrive - St. Clair College\WestfallDanaWeb315Assignment\danaWestfallGroceryApp\_Imports.razor"
+#line 5 "c:\Users\danaw\OneDrive - St. Clair College\WestfallDanaWeb315Assignment\danaWestfallGroceryApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\danaw\OneDrive - St. Clair College\WestfallDanaWeb315Assignment\danaWestfallGroceryApp\_Imports.razor"
+#line 6 "c:\Users\danaw\OneDrive - St. Clair College\WestfallDanaWeb315Assignment\danaWestfallGroceryApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\danaw\OneDrive - St. Clair College\WestfallDanaWeb315Assignment\danaWestfallGroceryApp\_Imports.razor"
+#line 7 "c:\Users\danaw\OneDrive - St. Clair College\WestfallDanaWeb315Assignment\danaWestfallGroceryApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\danaw\OneDrive - St. Clair College\WestfallDanaWeb315Assignment\danaWestfallGroceryApp\_Imports.razor"
+#line 8 "c:\Users\danaw\OneDrive - St. Clair College\WestfallDanaWeb315Assignment\danaWestfallGroceryApp\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\danaw\OneDrive - St. Clair College\WestfallDanaWeb315Assignment\danaWestfallGroceryApp\_Imports.razor"
+#line 9 "c:\Users\danaw\OneDrive - St. Clair College\WestfallDanaWeb315Assignment\danaWestfallGroceryApp\_Imports.razor"
 using danaWestfallGroceryApp;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\danaw\OneDrive - St. Clair College\WestfallDanaWeb315Assignment\danaWestfallGroceryApp\_Imports.razor"
+#line 10 "c:\Users\danaw\OneDrive - St. Clair College\WestfallDanaWeb315Assignment\danaWestfallGroceryApp\_Imports.razor"
 using danaWestfallGroceryApp.Shared;
 
 #line default
